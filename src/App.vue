8be7f8dfae4560ac5d18c318a2e43a5d1383eb96
Meike <meike_hankewicz@yahoo.de>
2018-09-06 14:01:58 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,134 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="todoList">
+    <h1 class="header"><i class="fab fa-vuejs"></i><span>Vue</span> To Do List</h1>
+
+    <div class="container">
+
+      <div class="input">
+            <input type="text" v-model="newItem" v-on:keyup.enter="addItem"/>
+            <button class="add" v-on:click="addItem">+</button> <!-- @click for shorthand -->
+      </div>
+
+      <div class="todos">
+        
+          <ToDoItem v-for="todo in todos" :key="todo" :todo="todo" @delete="deleteItem" />
+        
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from './components/ToDoItem.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ToDoItem
+  },
+  data(){
+    return {
+      todos: [],
+      newItem: ""
+    }
+  },
+
+  methods: {
+    addItem(){
+      if(this.newItem == ""){
+        return;
+      }
+
+      this.todos.push(this.newItem);
+      this.newItem = "";
+    },
+
+    deleteItem(todo){
+      this.todos = this.todos.filter(item => item !== todo);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,700');
+
+body{
+  margin: 0;
+  padding: 0;
+  background: linear-gradient(to right, #11998e, #38ef7d);
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Source Sans Pro';
 }
+
+.todoList{
+  text-align: center;
+  border: 1px solid #00C387;
+  background:#384660;
+  width: 30vw;
+  min-height: 70vH;
+  height: auto;
+  box-shadow: 2px 3px 15px rgba(0, 0, 0, 0.5);
+  padding-bottom: 60px;
+}
+
+.header{
+  color: #00C387;
+  font-weight: 300;
+  margin: 70px auto 30px;
+}
+
+i{
+  margin-right: 10px;
+}
+
+span{
+  font-family: 'Source Sans Pro';
+  font-weight: 700;
+}
+
+.input{
+  display: flex;
+  justify-content: space-evenly;
+  margin-bottom: 20px;
+}
+
+input{
+  width: 60%;
+  padding: 5px;
+  font-size: 1em;
+  box-shadow: 1px 3px 20px 0px rgba(0, 0, 0, 0.3);
+  outline: none;
+}
+
+input:focus{
+  border-color: #00C387;
+}
+
+button{
+  outline: none;
+}
+
+.add{
+  color: #00C387;
+  font-size: 2em;
+  width: 40px;
+  height: 40px;
+  padding: 0px;
+  cursor: pointer;
+  background: #384660;
+  border-radius: 10px;
+  box-shadow: 1px 1px 1px #399aba;
+  border-color: #00C387;
+}
+
+.container{
+  width: 80%;
+  margin: 0 auto;
+}
+
 </style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="item">
+    <p class="todo">{{todo}}</p>
+    <button class="delete" v-on:click="deleteItem(todo)">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDoItem',
+  props: ['todo'],
+  methods: {
+    deleteItem(todo){
+      this.$emit('delete', todo)
+    }
+  }
+}
+</script>
+
+<style>
+.item{
+  display: flex;
+  align-items: center;
+}
+
+.todo{
+  background: #47597a;
+  color: white;
+  width: 90%;
+  padding: 15px;
+  margin-right: 20px;
+}
+
+.delete{
+  width: 35px;
+  height: 35px;
+  cursor: pointer;
+  background: #384660;
+  border-radius: 10px;
+  border-color: #ff6961;
+  color: #ff6961;
+  font-size: 18px;
+}
+</style>
+
+
